2a589984982f6163d172d3b73eaf2e1c9764d5cc
Evansj5 <jared.evans@lexisnexis.com>
2025-11-20 13:51:14 -0500

    Adds new component to contentType

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -2,5 +2,8 @@ import blockContent from './blockContent'
 import category from './category'
 import post from './post'
 import author from './author'
+import { heroBanner } from './components/heroBanner'
+import { heroCTA } from './partials/heroCTA'
+import { heroItem } from './partials/heroItem'
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [post, author, category, blockContent, heroBanner, heroCTA, heroItem]
